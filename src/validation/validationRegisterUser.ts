f7c1f30d138454ea00aa3fb88f58f3b90c675f0d
yurii <robka4an@gmail.com>
2024-12-25 15:48:17 +0200

    add validation phone

--- src/validation/validationRegisterUser.ts
+++ src/validation/validationRegisterUser.ts
@@ -5,8 +5,11 @@ export const registerValidation = Yup.object().shape({
   email: Yup.string()
     .email("Invalid email format")
     .required("Email is required"),
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required"),
-  phone: Yup.string().required("Phone number is required"),
+  password: Yup.string().required("Password is required"),
+  phone: Yup.string()
+    .matches(
+      /^0\d{9}$/,
+      "Phone number should be in format (example 0681234567)"
+    )
+    .required("Phone number is required"),
 });
